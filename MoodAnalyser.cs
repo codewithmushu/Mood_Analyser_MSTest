08c55b11f49017b12935b3e85fe8af5cbe506435
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-09 00:05:07 +0530

    [Muskan] Added : Given a Message, ability to analyse and respond Happy or Sad Mood - Create MoodAnalyser Object - Call analyseMood function with message as parameter and return Happy or Sad Mood

---
+++ MoodAnalyser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class Mood
+    {
+        private string message;
+
+        public Mood(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            if (message.Contains("happy"))
+            {
+                return "Happy";
+            }
+            else if (message.Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Unknown";
+            }
+        }
+    }
+}
