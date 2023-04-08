eed0a2d479d113f0ac0f548f23fd5cd9a5ee3d22
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-09 00:29:19 +0530

    [Muskan] Added : analyseMood method can just return SAD to pass this Test Case (TC)

---
+++ MoodTest.cs
@@ -0,0 +1,17 @@
+using MoodAnalyser;
+
+namespace MoodAnalyserTest
+{
+    [TestClass]
+    public class MoodTest
+    {
+        [TestMethod]
+        public void TestAnalyseMoodSad()
+        {
+            Mood analyser = new Mood("I am in Sad Mood");
+            string mood = analyser.AnalyseMood();
+            Assert.AreEqual("SAD", mood);
+        }
+    }
+}      
+
