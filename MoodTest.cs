02b125c17a7dd75c02dad14101409f781c978720
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-09 13:19:35 +0530

    [Muskan] Added : Given “I am in Any Mood” message Should Return HAPPY

--- MoodTest.cs
+++ MoodTest.cs
@@ -12,6 +12,15 @@ namespace MoodAnalyserTest
             string mood = analyser.AnalyseMood();
             Assert.AreEqual("SAD", mood);
         }
+
+        [TestMethod]
+        public void TestAnalyseMoodHappy()
+        {
+            Mood analyser = new Mood("I am in Any Mood");
+            string mood = analyser.AnalyseMood();
+            Assert.AreEqual("HAPPY", mood); 
+        }
+
     }
 }      
 
